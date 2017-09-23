6acd0f3f4898b160673e8ec14864208f26283788
Victor Catão <victorcatao@hotmail.com>
2017-09-23 20:08:05 -0300

    pega fazani

--- public/javascript/controllers/login.js
+++ public/javascript/controllers/login.js
@@ -5,8 +5,18 @@ serviceLogin.service('usuarioService', function($http) {
   var usuarios = {};
 
   this.getStates = function(callback) {
-    //$http.get('states.json').success(callback);
-    $http.get('https://do-my-tattoo.herokuapp.com/account/getProfile/59c5a717b68662b7beb0912b').success(callback);
+
+    $http({
+      method: 'GET',
+      url: 'https://do-my-tattoo.herokuapp.com/account/getProfile/59c5a717b68662b7beb0912b'
+    }).then(function (success){
+      console.log(success);
+      callback(success);
+    },function (error){
+      console.log(error);
+      callback(error);
+    });
+    
   };
 });
 
@@ -14,6 +24,7 @@ serviceLogin.service('usuarioService', function($http) {
 serviceLogin.controller('loginCtrl', function($scope, usuarioService) {
 
   usuarioService.getStates(function(data){
+    console.log('retornou');
     $scope.usuarios = data;
   });
 
